8f876f70dd3758d3d108ed8793aeb80bf16b3220
Adeyinka M. Adefolurin <folurinyinka@gmail.com>
2020-09-15 22:17:42 +0100

    Update tools.js
    Add color codes to each tool for proper styling

--- src/store/tools.js
+++ src/store/tools.js
@@ -6,7 +6,40 @@ Vue.use(Vuex)
 export default {
 	namespaced: true,
 	state: {
-		tools: ['laravel', 'vuejs', 'git', 'vuetify', 'flutter', 'aws', 'vultr']
+		tools: [
+			{
+				name: 'laravel',
+				color: '#FF3326'
+			},
+			{
+				name: 'vuejs',
+				color: '#41B883'
+			},
+			{
+				name: 'git',
+				color: '#E84D31'
+			},
+			{
+				name: 'vuetify',
+				color: '#1764BA'
+			},
+			{
+				name: 'flutter',
+				color: '#51BFF0'
+			},
+			{
+				name: 'aws',
+				color: '#EF931E'
+			},
+			{
+				name: 'vultr',
+				color: '#0078F4'
+			},
+			{
+				name: 'cPanel',
+				color: '#E55E26'
+			}
+		]
 	},
 	mutations: {},
 	getters: {
